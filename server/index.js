c7e32ad94a117eb49154569a6f73f75dc1322eb7
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-07-16 18:32:24 -0400

    add comment to server/index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 const PORT = process.env.PORT || 3000;
 
+//To prevent server routing - react-router will handle the routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'))
 })
