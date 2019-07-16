5a3375a0ce71de75f7d4553fe729bd7a745e40e3
Andrew <Andrewkn09@Gmail.com>
2019-07-16 17:52:02 -0400

    Update puppeteer

--- __tests__/end-to-end/overview/overview.test.js
+++ __tests__/end-to-end/overview/overview.test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 let browser, page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({ headless: true });
+  browser = await puppeteer.launch({ headless: false });
   page = await browser.newPage();
   await page.goto('http://localhost:3000/');
 });
@@ -16,3 +16,8 @@ test('test home page url', async () => {
   const url = await page.url();
   expect(url).toBe('http://localhost:3000/');
 });
+
+// test('test cmoponent', async () => {
+//   const test = await page.$$eval('.test');
+//   expect(test).toBe('Vibranium');
+// });
