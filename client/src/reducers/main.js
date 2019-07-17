e4ce38c4a29b9e73842bcaa4e08803fbf0bfa607
Soumith Inturi <sintur2527@gmail.com>
2019-07-17 12:06:52 -0400

    Added actions and reducers to get product and product styles

--- client/src/reducers/main.js
+++ client/src/reducers/main.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 import IdReducer from './IdReducer.js';
+import productReducer from './overview/productReducer.js';
+import styleReducer from './overview/styleReducer.js';
 
 const rootReducer = combineReducers({
-  productId: IdReducer
+  productId: IdReducer,
+  product: productReducer,
+  styles: styleReducer,
 });
+
 export default rootReducer;
