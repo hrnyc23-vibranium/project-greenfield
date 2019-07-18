8a4c5520c624f2b2689b1a8c0df0048cd56152c0
Soumith Inturi <sintur2527@gmail.com>
2019-07-18 10:08:24 -0400

    Fixed product details page

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -5,7 +5,6 @@ import Reviews from './Reviews/Reviews.jsx';
 const ProductDetails = ({ match }) => {
   return (
     <Fragment>
-      <h3>ID: {match.params.id}</h3>
       <Overview />
       <Reviews />
     </Fragment>
