e77f7644e87130d09dd76098b427a14d4e30f89c
Soumith Inturi <sintur2527@gmail.com>
2019-07-17 23:11:54 -0400

    Added icon to navbar

--- client/src/components/overview/Header.jsx
+++ client/src/components/overview/Header.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  homeButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h2" color="inherit">
+          <IconButton
+            edge="start"
+            className={classes.homeButton}
+            color="inherit"
+            aria-label="Open drawer">
+            <HomeIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
             Vibranium
           </Typography>
         </Toolbar>
