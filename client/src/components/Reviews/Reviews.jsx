53a25de15caf18739c6144b2e733c0126f5b8649
Andrew <Andrewkn09@Gmail.com>
2019-07-17 22:25:29 -0400

    Add grid to separate Meta and ReviewsList

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import ReviewsList from './ReviewsList.jsx';
+import Grid from '@material-ui/core/Grid/';
+import Meta from './Meta.jsx';
 export default class Reviews extends Component {
   render() {
     return (
-      <div>
-        Review
-        <ReviewsList />
-      </div>
+      <Grid container>
+        <Grid item l={2}>
+          <Grid container spacing={10}>
+            <Grid item>
+              <Meta />
+            </Grid>
+            <Grid item>
+              <ReviewsList />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
