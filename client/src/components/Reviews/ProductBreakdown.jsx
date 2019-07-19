4e425c3513fdb1aade03a669a082ba55472cf4a1
Andrew <Andrewkn09@Gmail.com>
2019-07-19 13:21:02 -0400

    Add sliders

--- client/src/components/Reviews/ProductBreakdown.jsx
+++ client/src/components/Reviews/ProductBreakdown.jsx
@@ -3,13 +3,36 @@ import { connect } from 'react-redux';
 
 import * as actions from '../../actions/Reviews/getData.js';
 class ProductBreakdown extends Component {
+  renderBreakdown(breakdown) {
+    return Object.keys(breakdown).map(category => {
+      let number = Number(breakdown[category]) * 10;
+      return (
+        <div key={category}>
+          {category}
+          <div>
+            <input
+              type="range"
+              min="0"
+              max="50"
+              readOnly
+              className="slider"
+              value={number}
+            />
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.breakdown);
-    return (
+    const { breakdown } = this.props;
+    return breakdown ? (
       <div>
         ProductBreakdown
-        <div>size</div>
+        {this.renderBreakdown(breakdown)}
       </div>
+    ) : (
+      <div>Loading...</div>
     );
   }
 }
