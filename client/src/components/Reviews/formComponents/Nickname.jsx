6615b931d04889595d38ae06a2df641fc4394ec0
Andrew <Andrewkn09@Gmail.com>
2019-07-20 15:08:40 -0400

    Add Nickname input to form

---
+++ client/src/components/Reviews/formComponents/Nickname.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    width: 400
+  }
+}));
+
+const Nickname = ({ form, setForm }) => {
+  const classes = useStyles();
+
+  const handleChange = e => {
+    e.persist();
+    setForm(prevState => {
+      return { ...prevState, nickname: e.target.value };
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <TextField
+        className={classes.textField}
+        required={true}
+        inputProps={{ maxLength: 60 }}
+        onChange={handleChange}
+        placeholder="Example: Captain America"
+        required={true}
+        value={form.nickname}
+      />
+      <span>
+        For privacy reasons, do not use your full name or email address
+      </span>
+    </React.Fragment>
+  );
+};
+
+export default Nickname;
