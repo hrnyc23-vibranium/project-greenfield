327a9107d325ac0e8d89937e89910b7957a55dd7
Andrew <Andrewkn09@Gmail.com>
2019-07-21 18:24:29 -0400

    Add style

--- client/src/components/Reviews/ReviewsEntry.jsx
+++ client/src/components/Reviews/ReviewsEntry.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 // Material UI Components
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 //React Components
 import * as actions from '../../actions/Reviews/updateHelpful.js';
@@ -11,8 +12,37 @@ import Ratings from '../Ratings.jsx';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    display: 'block',
     borderBottom: '1px solid gray',
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(4),
+  },
+  body: {
+    marginBottom: theme.spacing(3),
+  },
+  images: {
+    marginBottom: theme.spacing(3),
+  },
+  image: {
+    marginRight: theme.spacing(1),
+    width: 100,
+    height: 100,
+  },
+  response: {
+    display: 'block',
+    background: '#D9D9D9',
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(3, 2),
+  },
+  responseTitle: {
+    fontWeight: 'bold',
+    margin: theme.spacing(0),
+    marginBottom: theme.spacing(2),
+  },
+  responseBody: {
+    margin: theme.spacing(0),
+  },
+  helpful: {
+    marginBottom: theme.spacing(1),
   },
 }));
 
@@ -22,26 +52,27 @@ const ReviewsEntry = ({ review, updateHelpful }) => {
   const renderPhotos = photos => {
     if (photos.length > 0) {
       return (
-        <div>
+        <Box className={classes.images}>
           {photos.map(photo => {
             return (
-              <img
-                key={photo.id}
-                src={photo.url}
-                style={{ width: '150px', height: 'auto' }}
-              />
+              <img key={photo.id} src={photo.url} className={classes.image} />
             );
           })}
-        </div>
+        </Box>
       );
     }
   };
 
+  const renderRecommend = recommend => {
+    return recommend ? <span>I recommend this product</span> : '';
+  };
+
   const renderResponse = response => {
     return response ? (
-      <span>
-        Response: <br /> {response}
-      </span>
+      <Box className={classes.response}>
+        <p className={classes.responseTitle}>Response:</p>
+        <p className={classes.responseBody}>{response}</p>
+      </Box>
     ) : (
       ''
     );
@@ -53,23 +84,28 @@ const ReviewsEntry = ({ review, updateHelpful }) => {
 
   return (
     <Box className={classes.root}>
-      <Ratings rating={review.rating} />
-      <span>
-        {review.reviewer_name}, {review.date}
-      </span>
-      <h5>{review.reviewer_name}</h5>
-      <p>{review.body}</p>
-      {review.recommend ? <span>I recommend this product</span> : ''}
-      {renderResponse(review.response)}
-      <br />
+      <Grid container direct="row" justify="space-between">
+        <Ratings rating={review.rating} />
+        <Box>
+          {review.reviewer_name}, {review.date}
+        </Box>
+      </Grid>
+      <h3 className={classes.summary}>{review.summary}</h3>
+      <p className={classes.body}>{review.body}</p>
+
       {renderPhotos(review.photos)}
-      <span>
+
+      {renderRecommend(review.recommend)}
+
+      {renderResponse(review.response)}
+
+      <Box className={classes.helpful}>
         <span>Helpful?</span>
         <button onClick={handleHelpful.bind(this, review.review_id)}>
           Yes({review.helpfulness})
         </button>
         <button>Report</button>
-      </span>
+      </Box>
       <br />
     </Box>
   );
