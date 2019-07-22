b8c98120545d97911797b6929ce3e9e9c0843824
Soumith Inturi <sintur2527@gmail.com>
2019-07-21 23:30:12 -0400

    Fixed issue with button render on carousel

--- client/src/components/overview/Carousel.jsx
+++ client/src/components/overview/Carousel.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 // Material UI Components
-import Box from '@material-ui/core/Box';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 import NextArrow from '@material-ui/icons/KeyboardArrowRightRounded';
 import BackArrow from '@material-ui/icons/KeyboardArrowLeftRounded';
-import Slide from '@material-ui/core/Slide';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,16 +19,17 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: '#E0E0E0',
     marginTop: theme.spacing(1),
   },
-  arrow: {
-    marginTop: '375px',
+  imgBox: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    maxWidth: '90%',
+    maxHeight: '100%',
   },
   img: {
     display: 'flex',
     maxWidth: '100%',
     maxHeight: '100%',
     objectFit: 'cover',
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
   },
   progress: {
     margin: theme.spacing(1),
@@ -61,38 +62,46 @@ const Carousel = props => {
   };
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.arrow}>
-        <IconButton
-          disabled={previous}
-          onClick={() => {
-            setIndex(Math.max(index - 1, 0));
-            changePrevious(index);
-          }}>
-          <BackArrow />
-        </IconButton>
+    <div>
+      <Box className={classes.root}>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center">
+          <Box className={classes.arrow}>
+            <IconButton
+              disabled={previous}
+              onClick={() => {
+                setIndex(Math.max(index - 1, 0));
+                changePrevious(index);
+              }}>
+              <BackArrow />
+            </IconButton>
+          </Box>
+          <Box className={classes.imgBox}>
+            {props.styles.results ? (
+              <img
+                src={props.styles.results[props.index].photos[index].url}
+                className={classes.img}
+              />
+            ) : (
+              <CircularProgress className={classes.progress} />
+            )}
+          </Box>
+          <Box className={classes.arrow}>
+            <IconButton
+              disabled={next}
+              onClick={() => {
+                setIndex(Math.min(index + 1, 5));
+                changeNext(index);
+              }}>
+              <NextArrow />
+            </IconButton>
+          </Box>
+        </Grid>
       </Box>
-      <Slide direction="left" in={true} mountOnEnter unmountOnExit>
-        {props.styles.results ? (
-          <img
-            src={props.styles.results[props.index].photos[index].url}
-            className={classes.img}
-          />
-        ) : (
-          <CircularProgress className={classes.progress} />
-        )}
-      </Slide>
-      <Box className={classes.arrow}>
-        <IconButton
-          disabled={next}
-          onClick={() => {
-            setIndex(Math.min(index + 1, 5));
-            changeNext(index);
-          }}>
-          <NextArrow />
-        </IconButton>
-      </Box>
-    </Box>
+    </div>
   );
 };
 
