4413bf718cd64752ad278c10fc5e1aeaae308d13
Andrew <Andrewkn09@Gmail.com>
2019-07-22 15:38:16 -0400

    Add validation and snackbar

---
+++ client/src/components/validation.js
@@ -0,0 +1,30 @@
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+export const validate = (form, component) => {
+  let errors = [];
+
+  if (component === 'reviews') {
+    if (!form.rating) errors.push('an overall rating');
+
+    if (!form.recommend) errors.push('a recommended input');
+
+    if (!form.name) errors.push('a nickname');
+
+    if (form.body.length < 50) errors.push('a review body over 50 characters');
+
+    if (!emailIsValid(form.email)) errors.push('a valid email address');
+
+    //check if characteristics exist
+    //check if images are valid
+  } else {
+    if (!form.question) errors.push('a question');
+
+    if (!form.name) errors.push('a nickname');
+
+    if (!emailIsValid(form.email)) errors.push('a valid email address');
+  }
+
+  return errors.length > 0 ? errors : false;
+};
