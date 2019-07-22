f049679acdb6a4386b4f3871397a685f7a248b38
Soumith Inturi <sintur2527@gmail.com>
2019-07-21 20:04:41 -0400

    Added carousel buttons

--- client/src/components/overview/Carousel.jsx
+++ client/src/components/overview/Carousel.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Material UI Components
 import Box from '@material-ui/core/Box';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import NextArrow from '@material-ui/icons/KeyboardArrowRightRounded';
+import BackArrow from '@material-ui/icons/KeyboardArrowLeftRounded';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,6 +17,9 @@ const useStyles = makeStyles(theme => ({
     height: '750px',
     backgroundColor: '#E0E0E0',
     marginTop: theme.spacing(1),
+  },
+  arrow: {
+    marginTop: '375px',
   },
   img: {
     display: 'flex',
@@ -30,13 +36,33 @@ const useStyles = makeStyles(theme => ({
 
 const Carousel = props => {
   const classes = useStyles();
+  console.log('props', props);
+
+  const [index, setIndex] = useState(0);
+
   return (
     <Box className={classes.root}>
+      <Box className={classes.arrow}>
+        <IconButton
+          onClick={() => {
+            setIndex(Math.max(index - 1, 0));
+          }}>
+          <BackArrow />
+        </IconButton>
+      </Box>
       {props.styles.results ? (
         <img src={props.img} className={classes.img} />
       ) : (
         <CircularProgress className={classes.progress} />
       )}
+      <Box className={classes.arrow}>
+        <IconButton
+          onClick={() => {
+            setIndex(Math.min(index + 1, 5));
+          }}>
+          <NextArrow />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
