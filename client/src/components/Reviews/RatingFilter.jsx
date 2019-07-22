c5a892095424bcd5efe06d03077f26dbbe753f8f
Andrew <Andrewkn09@Gmail.com>
2019-07-22 19:26:18 -0400

    Refactor to functional component

--- client/src/components/Reviews/RatingFilter.jsx
+++ client/src/components/Reviews/RatingFilter.jsx
@@ -8,29 +8,26 @@ import * as actions from '../../actions/Reviews/getData.js';
 import RatingBar from './RatingBar.jsx';
 import Ratings from '../Ratings.jsx';
 
-class RatingFilter extends Component {
+const RatingFilter = props => {
   //calculate percent recommended out of total reviews
-  normalizeRecommended(curr, total) {
+  const normalizeRecommended = (curr, total) => {
     if (total === 0) {
       return 0;
     }
     return (curr / total) * 100;
-  }
+  };
 
   //render out recommended percentage
-  renderRecommended(totalReviews) {
-    const { recommended } = this.props;
+  const renderRecommended = totalReviews => {
+    const { recommended } = props;
 
-    let normalized = this.normalizeRecommended(
-      recommended[1] || 0,
-      totalReviews
-    );
+    let normalized = normalizeRecommended(recommended[1] || 0, totalReviews);
     return <div>{normalized}% of reviews recommend this product</div>;
-  }
+  };
 
   //render out star rating
-  renderAvgRating(totalReviews) {
-    const { ratings } = this.props;
+  const renderAvgRating = totalReviews => {
+    const { ratings } = props;
     if (ratings && totalReviews > 0) {
       let totalStars = 0;
       for (let stars in ratings) {
@@ -41,18 +38,18 @@ class RatingFilter extends Component {
       return Number(avgRating.toFixed(1));
     }
     return 0;
-  }
+  };
 
   //if avg rating doesn't have a decimal, add one
-  padAvgRating(avgRating) {
+  const padAvgRating = avgRating => {
     let stringified = avgRating.toString();
     if (stringified.indexOf('.') < 0) {
       return stringified + '.0';
     }
     return stringified;
-  }
+  };
 
-  addTotal(ratings = {}) {
+  const addTotal = (ratings = {}) => {
     if (Object.keys(ratings).length > 0) {
       let totalReviews = Object.values(ratings).reduce((sum, num) => {
         return sum + num;
@@ -60,31 +57,27 @@ class RatingFilter extends Component {
       return totalReviews;
     }
     return 0;
-  }
+  };
 
-  render() {
-    const { recommended, ratings } = this.props;
-    const totalReviews = this.addTotal(ratings);
-    const avgRating = this.renderAvgRating(totalReviews);
+  const { recommended, ratings } = props;
+  const totalReviews = addTotal(ratings);
+  const avgRating = renderAvgRating(totalReviews);
 
-    return recommended && ratings ? (
-      <Box>
-        <Grid container direction="row" style={{ marginBottom: 13 }}>
-          <span
-            style={{ fontSize: 30, fontWeight: 'bold', marginRight: '15px' }}
-          >
-            {this.padAvgRating(avgRating)}
-          </span>
-          <Ratings rating={avgRating} />
-        </Grid>
-        {this.renderRecommended(totalReviews)}
-        <RatingBar ratings={ratings} totalReviews={totalReviews} />
-      </Box>
-    ) : (
-      <div>Loading...</div>
-    );
-  }
-}
+  return recommended && ratings ? (
+    <Box>
+      <Grid container direction="row" style={{ marginBottom: 13 }}>
+        <span style={{ fontSize: 30, fontWeight: 'bold', marginRight: '15px' }}>
+          {padAvgRating(avgRating)}
+        </span>
+        <Ratings rating={avgRating} />
+      </Grid>
+      {renderRecommended(totalReviews)}
+      <RatingBar ratings={ratings} totalReviews={totalReviews} />
+    </Box>
+  ) : (
+    <div>Loading...</div>
+  );
+};
 
 let mapStateToProps = state => ({
   productId: state.productId,
