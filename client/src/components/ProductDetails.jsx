1094199fb40169207e675637cb20771bb1799f1b
Andrew <Andrewkn09@Gmail.com>
2019-07-22 19:19:45 -0400

    Fix button sizing

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -15,7 +15,7 @@ class ProductDetails extends Component {
     return (
       <Fragment>
         <Overview />
-        {/* <Questions /> */}
+        <Questions />
         <Reviews />
       </Fragment>
     );
