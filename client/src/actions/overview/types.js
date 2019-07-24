6b0ed064cf0f131792f6e662ec1063250912be5b
Soumith Inturi <sintur2527@gmail.com>
2019-07-24 14:30:18 -0400

    Added cart actions

--- client/src/actions/overview/types.js
+++ client/src/actions/overview/types.js
@@ -1,4 +1,5 @@
 export const GET_PRODUCT_STYLES = 'get_product_styles';
 export const GET_PRODUCT = 'get_product';
 export const GET_CART = 'get_cart';
-export const GET_ALL_PRODUCTS = 'get_all_products';
+export const ADD_ITEM = 'add_item';
+export const REMOVE_ITEM = 'remove_item';
