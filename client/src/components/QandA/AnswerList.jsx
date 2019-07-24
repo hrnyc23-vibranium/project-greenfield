4711eddf3322d4e471df1388827830cacc2bc2a5
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-07-24 11:14:13 -0400

    load more answers on click'

--- client/src/components/QandA/AnswerList.jsx
+++ client/src/components/QandA/AnswerList.jsx
@@ -3,14 +3,23 @@ import React from 'react';
 import Answer from './Answer';
 import axios from 'axios';
 
+//Material UI
+import { Button } from '@material-ui/core';
+
 class Answers extends React.Component {
   constructor(props) {
     super(props);
     this.state = { answers: [], load: 2 };
     this.getAnswers = this.getAnswers.bind(this);
+    this.loadMore = this.loadMore.bind(this);
   }
   componentDidMount() {
     this.getAnswers();
+  }
+  loadMore() {
+    this.setState({
+      load: this.state.load + 2,
+    });
   }
 
   getAnswers(page = 1, count = 50, keyword = null) {
@@ -37,10 +46,22 @@ class Answers extends React.Component {
               return <Answer answer={answer} key={index} />;
             }
           })}
+
+          {this.state.load < this.state.answers.length ? (
+            <Button
+              variant="text"
+              onClick={() => {
+                this.loadMore();
+              }}>
+              Load more answers
+            </Button>
+          ) : (
+            <div />
+          )}
         </div>
       );
     } else {
-      return <div>answer not rendered</div>;
+      return <div>There's not answer for this question yet</div>;
     }
   }
 }
