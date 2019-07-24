341ead85885059e86545a9ad5ae9e69062c3304e
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-07-24 09:58:21 -0400

    pass product name to answer form

--- client/src/components/QandA/Question.jsx
+++ client/src/components/QandA/Question.jsx
@@ -63,7 +63,7 @@ const Question = props => {
             </Button>
             <Typography component="h4"> | </Typography>
             <AnswerForm
-              product={'something'}
+              product={props.product}
               question={props.question.question_body}
               questionId={props.question.question_id}
             />
